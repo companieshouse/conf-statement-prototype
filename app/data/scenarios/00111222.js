bb803eea161639747349744b802d971941dc61a7
Oceanne Esparcieux <oesparcieux@companieshouse.gov.uk>
2021-01-12 12:40:37 +0000

    ch page commit cs dates added

--- app/data/scenarios/00111222.js
+++ app/data/scenarios/00111222.js
@@ -10,7 +10,9 @@ module.exports = {
     address: '125 Feather Hedge<br>Salisbury<br>SP4 7DE',
     sailAddress: '2 Second Place<br>Portsmouth<br>PO2 2BB',
     actionCode: '5005',
+    csDate: '2 February 2021',
     csDueDate: '16 February 2021',
+    csLast: '16 February 2020',
     csDue: 'notdue',
     nextCsDue: '16 February 2022'
   }
