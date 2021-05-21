4c677e8800f7d8543c1675db3ec3d1431d333244
Oceanne Esparcieux <oesparcieux@companieshouse.gov.uk>
2021-05-21 15:45:29 +0100

    PSC card and 5 director scenario commit

---
+++ app/data/scenarios/00222335.js
@@ -0,0 +1,26 @@
+module.exports = {
+  company: {
+    name: 'FIVE FRIENDS LTD',
+    number: '00222335',
+    authCode: '222222',
+    type: 'LTD',
+    status: 'Active',
+    incorporationDate: '1 February 2019',
+    companyType: 'Private Limited Company',
+    address: '5 Forever Street\nSalisbury\nSP4 7DE',
+    sailAddress: '2 Second Place\nPortsmouth\nPO2 2BB',
+    actionCode: '5005',
+    csDate: '30 April 2021',
+    csDueDate: '14 May 2021',
+    csLast: '26 March 2020',
+    csDue: 'due',
+    nextCsDue: '30 April 2022',
+    directors: '5',
+    pscs: '5',
+    register: 'ro',
+    shareholders: '5',
+    tradingStatus: '1',
+    recentFiling: 'yes',
+    pscExempt: '0'
+  }
+}
