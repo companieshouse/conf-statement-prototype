bd28693980ce3d51b6e29afde25b6c24babb0ea4
Oceanne Esparcieux <oesparcieux@companieshouse.gov.uk>
2021-07-08 08:46:20 +0100

    link commit

--- app/routes.js
+++ app/routes.js
@@ -366,7 +366,7 @@ if ('taskList' == true) {
   switch (ro) {
     case 'yes':
       checked.yes = true
-      res.redirect('/confirmation-statement/registers')
+      res.redirect('/confirmation-statement/task-list')
       break
     case 'no':
       checked.yes = true
